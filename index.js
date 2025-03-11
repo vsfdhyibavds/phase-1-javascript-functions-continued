044783717c999e0ad3ba8a62017791b8b3237707
Eugene Wekesa <eugenco578@gmail.com>
2025-03-11 19:16:48 +0300

    function declaration, invocation, and function scope

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+// saturdayFun
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+
+  // mondayWork
+  function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+
+  // wrapAdjective
+  function wrapAdjective(flair = "*") {
+    return function (adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+
+  
